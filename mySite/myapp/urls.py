be3d546848fa64b5346b2db959554c105ba6c857
SanamChoudhary <sanamc541@gmail.com>
2025-03-30 23:25:04 -0500

    Fixed home url, changed images in home

--- mySite/myapp/urls.py
+++ mySite/myapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create, name='create'),
     path('view/', views.view, name='view'),
-    path('functions/', views.functions, name='functions'),  # Ensure this is included
+    path('functions/', views.functions, name='functions'),
+    path('home/', views.home, name='home')  # Ensure this is included
 ]
